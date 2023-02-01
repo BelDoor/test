022851e1e9a3d53da95c5b8b936ae2fe5d879584
Pawel Darafeyeu <dorofeevforgit@gmail.com>
2023-02-01 17:21:03 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(RandomStringUtils.random(5,true,false));
+        System.out.println(RandomStringUtils.random(10,true,true));
+    }
+}
